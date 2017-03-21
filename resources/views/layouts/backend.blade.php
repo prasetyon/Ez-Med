9b079a5a0c702e83eddc8c610c3f0ef56d463ebe
prasetyon <prstyngrhd@gmail.com>
2017-03-21 12:23:43 +0700

    backend

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -53,11 +53,6 @@
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
-
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Messages: style can be found in dropdown.less-->
