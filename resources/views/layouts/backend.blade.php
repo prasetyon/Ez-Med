32eb3cd4fc7428cf07bc10230e8003123f8fd050
stevenkur <steven_kurkur@yahoo.com>
2017-04-29 14:58:43 +0700

    fix view

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -53,9 +53,10 @@
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-      <div class="navbar-custom-menu">
-        
-      </div>
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
     </nav>
   </header>
   <!-- Left side column. contains the logo and sidebar -->
@@ -109,7 +110,7 @@
           </ul>
         </li>
          
-        <li class="treeview active">
+        <li class="treeview">
           <a href="#">
             <i class="fa fa-user-md"></i>
             <span>Doctor</span>
@@ -172,45 +173,5 @@
 </div>
 <!-- ./wrapper -->
 
-    
-  <!-- jQuery 2.2.3 -->
-<script src="admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.6 -->
-<script src="admin/bootstrap/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="admin/plugins/morris/morris.min.js"></script>
-<!-- ChartJS 1.0.1 -->
-<script src="admin/plugins/chartjs/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="admin/plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="admin/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="admin/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="admin/plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="admin/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="admin/plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="admin/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="admin/plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="admin/dist/js/app.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="admin/dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="admin/dist/js/demo.js"></script>
 </body>
 </html>
